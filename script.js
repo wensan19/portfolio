7d892e047841f29cbc76c8a78c93dd542f8cfbbd
wensan19 <wensan.25@ichat.sp.edu.sg>
2026-04-04 23:22:08 +0800

    Add portfolio code

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
+const faders = document.querySelectorAll(".fade-up");
 
+function showOnScroll() {
+  faders.forEach((item) => {
+    const rect = item.getBoundingClientRect();
+    if (rect.top < window.innerHeight - 80) {
+      item.classList.add("show");
+    }
+  });
+}
+
+window.addEventListener("scroll", showOnScroll);
+window.addEventListener("load", showOnScroll);
